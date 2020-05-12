0786eab3f7ce970d514098f9fda8c16d9c524dd1
brownguo <yexuan.guo@codedance.cn>
2020-05-12 17:50:12 +0800

    search fix

--- src/views/orders/OrderList.vue
+++ src/views/orders/OrderList.vue
@@ -6,6 +6,59 @@
         <span slot="tab">
           <a-icon type="ordered-list" />订单列表
         </span>
+
+        <div class="table-page-search-wrapper">
+          <a-form layout="inline">
+            <a-row :gutter="48">
+              <a-col :md="8" :sm="24">
+                <a-form-item label="规则编号">
+                  <a-input placeholder=""/>
+                </a-form-item>
+              </a-col>
+              <a-col :md="8" :sm="24">
+                <a-form-item label="使用状态">
+                  <a-select placeholder="请选择" default-value="0">
+                    <a-select-option value="0">全部</a-select-option>
+                    <a-select-option value="1">关闭</a-select-option>
+                    <a-select-option value="2">运行中</a-select-option>
+                  </a-select>
+                </a-form-item>
+              </a-col>
+              <template>
+                <a-col :md="8" :sm="24">
+                  <a-form-item label="调用次数">
+                    <a-input-number style="width: 100%"/>
+                  </a-form-item>
+                </a-col>
+                <a-col :md="8" :sm="24">
+                  <a-form-item label="更新日期">
+                    <a-date-picker style="width: 100%" placeholder="请输入更新日期"/>
+                  </a-form-item>
+                </a-col>
+                <a-col :md="8" :sm="24">
+                  <a-form-item label="使用状态">
+                    <a-select placeholder="请选择" default-value="0">
+                      <a-select-option value="0">全部</a-select-option>
+                      <a-select-option value="1">关闭</a-select-option>
+                      <a-select-option value="2">运行中</a-select-option>
+                    </a-select>
+                  </a-form-item>
+                </a-col>
+              </template>
+              <a-col :md="!advanced && 8 || 24" :sm="24">
+                  <span class="table-page-search-submitButtons" :style="advanced && { float: 'right', overflow: 'hidden' } || {} ">
+                    <a-button type="primary">查询</a-button>
+                    <a-button style="margin-left: 8px">重置</a-button>
+                    <a @click="toggleAdvanced" style="margin-left: 8px">
+                      {{ advanced ? '收起' : '展开' }}
+                      <a-icon :type="advanced ? 'up' : 'down'"/>
+                    </a>
+            </span>
+              </a-col>
+            </a-row>
+          </a-form>
+        </div>
+
         <a-table :columns="columns" :dataSource="data" :loading="tableLoding" :rowKey="record => record.path">
           <span slot="action" slot-scope="text, record">
             <a @click="onEdit(record)">更改</a>
@@ -59,6 +112,7 @@ export default {
       data: [],
       buttonLoading: false,
       tableLoding: true,
+      advanced: false,
       columns
     }
   },
@@ -83,6 +137,10 @@ export default {
       }).catch(err => {
         $message.error(`load user err: ${err.message}`)
       })
+    },
+    toggleAdvanced () {
+      this.advanced = !this.advanced
+      console.log(this.advanced)
     },
     onEdit (value) {
       console.log(JSON.stringify(value))
